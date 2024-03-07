990cd79d0b9eb8b6a4b21eb8125559bcea706a6d
Kubonka <srbetito85@gmail.com>
2024-03-07 16:02:59 -0300

    setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
-import AuthProvider from "@/context/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-	title: "Presupuestos Modista",
-	description: "Herramienta para generar y administrar presupuestos",
+	title: "Bills Sheduler",
+	description: "Sheduler",
 };
 
 export default async function RootLayout({
@@ -17,10 +15,7 @@ export default async function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.className} h-full flex-col flex gap-0 `}>
-				<AuthProvider>
-					<Toaster />
-					{children}
-				</AuthProvider>
+				{children}
 			</body>
 		</html>
 	);
