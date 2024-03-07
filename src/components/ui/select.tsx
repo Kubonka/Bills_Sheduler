85ec3b776073d3369f18e50a06a3f4d5854d38b0
Kubonka <srbetito85@gmail.com>
2024-03-07 16:05:12 -0300

    setup

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { useEffect, useState } from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
-import Overlay from "@/components/Overlay";
 
 import { cn } from "@/lib/utils";
 
