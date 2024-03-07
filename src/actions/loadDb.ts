0bd0a1acf816a41943a71cef4bdb285575e7a904
Kubonka <srbetito85@gmail.com>
2024-03-07 16:09:25 -0300

    setup

--- src/actions/loadDb.ts
+++ src/actions/loadDb.ts
@@ -1,14 +1,14 @@
-"use server";
-import DummyDb from "@/lib/db/dummyDb/DummyDb";
-export async function loadUnits() {
-	const dummyDb = new DummyDb();
-	//await dummyDb.reset();
-	await dummyDb.initBaseTables();
-	//await prisma.unit.create({ data: { singular: "hola", plural: "chau" } });
-}
-export async function loadBudgets() {
-	const dummyDb = new DummyDb();
-	//await dummyDb.reset();
-	await dummyDb.initBudgets();
-	//await prisma.unit.create({ data: { singular: "hola", plural: "chau" } });
-}
+// "use server";
+// import DummyDb from "@/lib/db/dummyDb/DummyDb";
+// export async function loadUnits() {
+// 	const dummyDb = new DummyDb();
+// 	//await dummyDb.reset();
+// 	await dummyDb.initBaseTables();
+// 	//await prisma.unit.create({ data: { singular: "hola", plural: "chau" } });
+// }
+// export async function loadBudgets() {
+// 	const dummyDb = new DummyDb();
+// 	//await dummyDb.reset();
+// 	await dummyDb.initBudgets();
+// 	//await prisma.unit.create({ data: { singular: "hola", plural: "chau" } });
+// }
